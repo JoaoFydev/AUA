fe5d3973e0b59366cf922737940e426bce7e5a44
Joao Fernandes <joao.fdias789@gmail.com>
2024-03-08 15:55:17 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet">
+</head>
+
+<body>
+    <title>AUA - Alugar um advogado</title>
+    <label class="close-menu-label" for="close-menu" title="close menu"></label>
+    <aside class="menu white-bg">
+        <div class="main-content menu-content">
+            <h1 onclick="getElementById('close-menu').checked = false;">
+                <a href="#home">LOGO</a>
+            </h1>
+
+            <nav>
+                <ul onclick="getElementById('close-menu').checked = false;">
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#grid-one">Sobre</a></li>
+                    <li><a href="#gird-two">Serviços</a></li>
+                    <li><a href="#grid-three">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
+
+
+    <div class="menu-spacing"></div>
+    <section id="home" class="intro main-bg section">
+        <div class="main-content intro-content">
+            <div class="intro-text-content">
+                <h2>PRECISANDO DE UM ADVOGADO"?</h2>
+                <p>Alugar um advogado</p>
+            </div>
+
+            <section id="intro" class="white-bg section">
+              <div class="main-content top3-content">
+                  <h2>Sobre nós</h2>
+                  <p>Our mission is to provide supportive environment where people of
+                    all fitness levels can achieve their health goals.
+                  </p>
+
+                  <p>At our center, we offer a wide range of equipment and amenities to help
+                     you get the most out of your workouts..</p>
+                  <p>But we're more than just a gym - we're a community of people who are committed
+                     to living a healthy and active lifestyle.</p>
+              </div>
+          </section>
+
+          <section id="grid-one" class="grid-one main-bg section">
+            <div class="main-content grid-one-content">
+                <h2 class="grid-main-heading">NOSSOS SERVIÇOS</h2>
+                <div class="grid">
+                    <article>
+                        <h3>DIREITO TRABALHISTA</h3>
+                        <p> But we're more than just a gym - we're a community of people who are committed to living a healthy and active lifestyle..</p>
+                    </article>
+                    <article>
+                        <h3>DIREITO DA FAMILIA</h3>
+                        <p>But we're more than just a gym - we're a community of people who are committed to living a healthy and active lifestyle.</p>
+                    </article>
+                    <article>
+                        <h3>DIREITO PENAL</h3>
+                        <p>Detalhes
+                          direito
+                           penal
+                          But we're more than just a gym - we're a community of people who are committed to living a healthy and active lifestyle.</p>
+                    </article>
+                </div>
+            </div>
+            </div>
+        </section>
+
+        <div class="menu-spacing"></div>
+    <section id="home" class="intro main-bg section">
+        <div class="main-content intro-content">
+            <div class="intro-text-content">
+                <h2>EXCELÊNCIA INTERNACIONAL</h2>
+                <p>This program is designed to help you achieve a total body transformation in just 8 weeks!
+                   Whether you're looking to lose weight, tone up, or improve your overall fitness,
+                   this program will help you reach your goals. Each week, you'll have 5 workouts that
+                    target different areas of the body, along with a recommended meal plan to support
+                     your progress.</p>
+                     <button>BOTÃO</button>
+            </div>
+          </section>
+
+          <div class="menu-spacing"></div>
+          <section id="home" class="intro main-bg section">
+              <div class="main-content intro-content">
+                  <div class="intro-text-content">
+                      <h1>PERGUNTAS FREQUENTES</h1>
+                      <h2>Lorem ipsum dolor sit amet, consectetur adipi</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipi</h2>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipi</h2>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipi</h2>
+                  </div>
+                </section>
+
+                <div class="intro-img">
+                  <img src="assets/javascript.svg" alt="Logo do HTML, CSS e JS">
+              </div>
+              <div class="contact-form">
+                  <fieldset class="form-grid">
+                      <legend>CONVERSE COM UM ADVOGADO</legend>
+
+                      <div class="form-group">
+                          <label for="first-name">First name
+                          </label>
+                          <input type="text" name="fisrt-name" id="fisrt-name" placeholder="Your name">
+                      </div>
+
+                      <div class="form-group">
+                          <label for="last-name">Last name
+                          </label>
+                          <input type="text" name="last-name" id="last-name" placeholder="Your last name">
+                      </div>
+
+                      <div class="form-group">
+                          <label for="email">E-mail
+                          </label>
+                          <input type="email" name="email" id="email" placeholder="Your e-mail">
+                      </div>
+
+                      <div class="form-group full-width">
+                          <label for="message">Message
+                          </label>
+                          <textarea name="message" id="message" cols="30" rows="10" placeholder="your message">
+                                          </textarea>
+                      </div>
+
+                      <div class="form-group full-width">
+                          <button type="submit">Send message
+                          </button>
+                      </div>
+                  </fieldset>
+              </div>
+          </div>
+      </section>
+
+      <footer id="footer" class="footer white-bg">
+          <p><a rel="nofollow" target="_blank" href="https://github.com/JoaoFGx">Feito com <span class="heart">♥</span> por
+                  João Victor Fernandes Dias</p>
+      </footer>
+</body>
+
+</html>
